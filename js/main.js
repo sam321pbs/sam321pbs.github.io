4312a9711e1ad6ca7b825165a42997453504e6df
Samuel Mengistu <sam321pbs@gmail.com>
2019-05-23 09:10:08 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$("#thanks-button").click( (item) => {
+   $("#thanks-button").addClass("spin");
+});
+
+$("#web_app_image").click(() => {
+   window.open ('./project-7/index.html','_blank',false);
+});
+
+$("#employee_directory_image").click(() => {
+   window.open ('./project-8/index.html','_blank',false);
+});
+
+$("#interactive_gallary_image").click(() => {
+   window.open ('./project-5/index.html','_blank',false);
+});
+
+$("#game_show_image").click(() => {
+   window.open ('./project-6/index.html','_blank',false);
+});
